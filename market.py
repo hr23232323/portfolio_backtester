0784330475dc612e487c6002468e22b0f969d9eb
Harsh Rana <hr23232323@gmail.com>
2020-03-02 21:19:45 -0500

    historic price data manipulation added

--- market.py
+++ market.py
@@ -3,10 +3,18 @@ import numpy as np
 
 ## Python file to represent the market. This module loads the historic data and gives access to the other project files
 class market:
-    def __init__(self):
-        self.historic_data = pd.read_csv("historic_data.csv")
 
-    
+    def price_option(self, row, ticker):
+        return 0
+
+    def __init__(self):
+        historic_data = pd.read_csv("historic_data.csv")
+        historic_data["QQQ_call_price"] = historic_data.apply(lambda row: self.price_option(row, "QQQ"), axis=1)
+        historic_data["VTI_call_price"] = historic_data.apply(lambda row: self.price_option(row, "VTI"), axis=1)
+        self.data =historic_data
+
+
+
 
 market = market()
-print(market.historic_data.head())
+print(market.data.head())
