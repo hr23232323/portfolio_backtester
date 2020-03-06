6f2f3fbcd7829802052ff24232b65093811acbf5
Harsh Rana <hr23232323@gmail.com>
2020-03-05 22:12:31 -0500

    trader and market interface connected

--- main.py
+++ main.py
@@ -3,13 +3,19 @@ from trader import trader
 
 def main():
     trade_market = market()
-    trader_1 = trader(10000, 2000)
+    trading_capital = 10000
+    position_size = 0.2
+    position_capital = trading_capital*position_size
+    trader_1 = trader(trading_capital, position_capital)
+    num_positions = 10
     ticker = "QQQ"
-    for i in range(5):
+    for i in range(num_positions):
         trader_1.open_rand_position(trade_market, ticker)
-        trader_1.print_status()
+        #trader_1.print_status()
         trader_1.close_position(trade_market)
         trader_1.print_status()
 
+    profit_percent = float("{0:.2f}".format(((trader_1.money-trading_capital)/float(10000))*100))
+    print("After " + str(num_positions) + " positions, profit %: " + str(profit_percent) + "%")
 if __name__ == "__main__":
     main()
